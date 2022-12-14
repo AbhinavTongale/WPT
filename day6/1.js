73cc7f47d2f2eb358b101425b4d5b15151a114fa
AbhinavTongale <100940186+AbhinavTongale@users.noreply.github.com>
2022-12-14 08:47:52 -0800

    Add files via upload

---
+++ day6/1.js
@@ -0,0 +1,19 @@
+function changeColor(p1color) {
+    let h1Ref = document.querySelector("#id1");
+    h1Ref.style.color = p1color;
+  }
+  
+  function changeFontSize() {
+    let h1Ref = document.querySelector("h1");
+    h1Ref.style.fontSize = "64px";
+  }
+  function changeFontSize1() {
+    let h1Ref = document.querySelector("h1");
+    h1Ref.style.fontSize = "32px";
+  }
+  
+  function reset() {
+    let h1Ref = document.querySelector("h1");
+    h1Ref.style.color = "inherit";
+    h1Ref.style.fontSize = "32px";
+  }
