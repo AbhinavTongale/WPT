8574cabf8956b44550ce947bdd4c4c1e4898341e
AbhinavTongale <100940186+AbhinavTongale@users.noreply.github.com>
2022-12-14 23:28:01 -0800

    Add files via upload

---
+++ day8/practice/index.js
@@ -0,0 +1,93 @@
+// console.log('helloWorld');
+
+// var myName = 'Abhinav Tongale';
+// var myAge = 23;
+
+
+// console.log(myName);
+// console.log(myAge);
+
+
+// var _myName = 'Abhinav Tongale';
+// console.log(_myName);
+
+
+// var _1myName = 'Abhinav Tongale';
+// console.log(_1myName);
+
+
+// var _1my_Name = 'Abhinav Tongale';
+// console.log(_1my_Name);
+
+
+// var myName = 'Abhinav Tongale';
+// console.log(myName);
+
+// console.log(typeof(myName));
+
+
+// var iamabhi = true;
+// console.log(iamabhi);
+
+// console.log(typeof(iamabhi) );
+
+
+// var iamabhi = false;
+// console.log(iamabhi);
+
+// console.log(typeof(iamabhi) );
+
+
+// console.log(10+10);
+
+// console.log("abhi "+"tongale");
+
+// console.log(""+"0");
+
+//  console.log("abhi "-"tongale");
+
+
+//  console.log(true+true);
+//  1 true
+//  2 false
+
+
+// var abhi;
+// console.log(abhi);
+
+// var myPhoneNumber = 9876543210;
+// var myName = "Abhinav";
+
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// if(isNaN(myName)){
+//     console.log("plz enter valid phone no");
+// }
+
+
+
+// var x = 5;
+// var y = 5;
+
+// console.log(`{x == y}`);
+
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(9/3);
+
+// console.log("Reminder Oprator"+ 61%5);
+
+
+
+// var num = 15;
+// var newNum = --num + 5;
+// console.log(num);
+// console.log(newNum);
+
+
+
+
+
